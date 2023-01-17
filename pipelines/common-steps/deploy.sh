465d7afe97177e4369dbe693b4390b0c0f3d610f
zetas1982 <antoniojimenezramirez30@gmail.com>
2023-01-17 18:02:17 +0100

    Update deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --resolve-s3
