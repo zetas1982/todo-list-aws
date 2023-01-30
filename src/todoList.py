da083aa2f18bfcea3d7c91bfb21a16e5052dc543
zetas1982 <antoniojimenezramirez30@gmail.com>
2023-01-30 20:08:52 +0100

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -8,6 +8,9 @@ from botocore.exceptions import ClientError
 
 
 def get_table(dynamodb=None):
+    if os.environ['ENDPOINT_OVERRIDE']
+        URL = os.environ['ENDPOINT_OVERRIDE']
+    
     if not dynamodb:
         URL = os.environ['ENDPOINT_OVERRIDE']
         if URL:
