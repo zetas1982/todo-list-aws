27bfc943bc27f6ef1c27603d6089dd84eca89058
zetas1982 <antoniojimenezramirez30@gmail.com>
2023-01-30 17:56:43 +0100

    Update unit_test.sh

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -8,5 +8,5 @@ export DYNAMODB_TABLE=todoUnitTestsTable
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
-coverage report
+coverage report -m
 coverage xml
