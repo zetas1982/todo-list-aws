76b66475f0c5df86cb7a9bebefbbc0b8dc7d5ac3
zetas1982 <antoniojimenezramirez30@gmail.com>
2023-01-31 20:03:25 +0100

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -8,23 +8,17 @@ from botocore.exceptions import ClientError
 
 
 def get_table(dynamodb=None):
-    try:
-        print("Estoy en get_table")
-        print(dynamodb)
-        if not dynamodb:
-            URL = os.environ['ENDPOINT_OVERRIDE']
-            if URL:
-                print(URL)
-                boto3.client = functools.partial(boto3.client,
-                                                 endpoint_url=URL)
-                boto3.resource = functools.partial(boto3.resource,
-                                                   endpoint_url=URL)
-            dynamodb = boto3.resource("dynamodb")
-        # fetch todo from the database
-        table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-        return table
-    except Exception as e:
-        print('Error en get_table: ' + str(e))
+    if not dynamodb:
+        URL = os.environ['ENDPOINT_OVERRIDE']
+        if URL:
+            print('URL dynamoDB:'+URL)
+            boto3.client = functools.partial(boto3.client, endpoint_url=URL)
+            boto3.resource = functools.partial(boto3.resource,
+                                               endpoint_url=URL)
+        dynamodb = boto3.resource("dynamodb")
+    # fetch todo from the database
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    return table
 
 
 def get_item(key, dynamodb=None):
